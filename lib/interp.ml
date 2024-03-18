fe40012132e8edad90c9e256bda4dfaf41571407
melody! <24357776+melody-notpond@users.noreply.github.com>
2024-03-18 15:46:02 +0000

    492 - started interp for match

--- lib/interp.ml
+++ lib/interp.ml
@@ -24,12 +24,66 @@ let destruct_type_func t : (ty * ty) interper =
   | TyFunc (a, r) -> return (a, r)
   | _ -> error "expected a function"
 
+let type_is_func t : bool =
+  match t with
+  | TyFunc _ -> true
+  | _ -> false
+
+let adt_of_value v : (string * value list * ty) interper=
+  match v with
+  | VAdtInst (name, args, t) -> return (name, args, t)
+  | _ -> error "expected adt"
+
 let rec eval_expr e : value interper =
   match e with
   | EId x -> lookup x
   | ELet {bind = _; bound = _; result = _} -> failwith "unimplemented"
   | ELetRec {bind = _; bound = _; and_binds = _; result = _} -> failwith "unimplemented"
-  | EMatch (_e, _branches) -> failwith "unimplemented"
+
+  | EMatch (e, branches) ->
+    let* v = eval_expr e in
+    let* (name, args, t) = adt_of_value v in
+    if type_is_func t then
+      error "cannot pattern match on partial constructor application"
+    else let rec find_branch branches : value interper =
+      match branches with
+      | [] -> error "unsatisfied pattern"
+      | (p, e) :: xs ->
+        let rec pat_matches p v : env option interper =
+          match p with
+          | PWild -> return (Some [])
+          | PIdent (x, []) ->
+            let* env = ask in begin
+            match List.assoc_opt x env with
+            | Some (VAdtInst (n, [], t)) when n = x ->
+              if n = name then
+                return (Some [])
+              else
+                return (Some [(x, v)])
+            | _ -> return (Some [(x, v)])
+            end
+          | PIdent (cons, ps) ->
+            if cons = name then
+              let rec helper ps args =
+                match ps, args with
+                | p :: ps, x :: xs ->
+                  let ()
+          | POr (p1, p2) ->
+            return @@
+            let (b, env) = pat_matches p1 v in
+            if b then
+              return (b, env)
+            else
+              pat_matches p2 v
+        in
+        let* (matches, env) = pat_matches p v in
+        if matches then
+          local (return @@ env) begin
+            eval_expr e
+          end
+        else
+          find_branch xs
+    in find_branch branches
   | ELam (x, _t, e) ->
     let* env = ask in
     return @@ VLam (x, env, e)
